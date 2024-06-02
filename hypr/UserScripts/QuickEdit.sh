d57932f6dcf1244dbbba98d945c09c326337db89
adii-b <adityabharadwaj273@gmail.com>
2024-06-02 21:50:40 +0530

    Changed fastfetch

--- hypr/UserScripts/QuickEdit.sh
+++ hypr/UserScripts/QuickEdit.sh
@@ -5,49 +5,49 @@ configs="$HOME/.config/hypr/configs"
 UserConfigs="$HOME/.config/hypr/UserConfigs"
 
 menu(){
-  printf "1. view Env-variables\n"
-  printf "2. view Window-Rules\n"
-  printf "3. view Startup_Apps\n"
-  printf "4. view User-Keybinds\n"
-  printf "5. view Monitors\n"
-  printf "6. view Laptop-Keybinds\n"
-  printf "7. view User-Settings\n"
-  printf "8. view Workspace-Rules\n"
-  printf "9. view Default-Settings\n"
-  printf "10. view Default-Keybinds\n"
+  printf "View Env-variables\n"
+  printf "View Window-Rules\n"
+  printf "View Startup_Apps\n"
+  printf "View User-Keybinds\n"
+  printf "View Monitors\n"
+  printf "View Laptop-Keybinds\n"
+  printf "View User-Settings\n"
+  printf "View Workspace-Rules\n"
+  printf "View Default-Settings\n"
+  printf "View Default-Keybinds\n"
 }
 
 main() {
-    choice=$(menu | rofi -i -dmenu -config ~/.config/rofi/config-compact.rasi | cut -d. -f1)
+    choice=$(menu | rofi -i -dmenu -config ~/.config/rofi/config-compact.rasi)
     case $choice in
-        1)
+        *"View Env-variables")
             kitty -e nvim "$UserConfigs/ENVariables.conf"
             ;;
-        2)
+        *"View Window-Rules")
             kitty -e nvim "$UserConfigs/WindowRules.conf"
             ;;
-        3)
+        *"View Startup_Apps")
             kitty -e nvim "$UserConfigs/Startup_Apps.conf"
             ;;
-        4)
+        *"View User-Keybinds")
             kitty -e nvim "$UserConfigs/UserKeybinds.conf"
             ;;
-        5)
+        *"View Monitors")
             kitty -e nvim "$UserConfigs/Monitors.conf"
             ;;
-        6)
+        *"View Laptop-Keybinds")
             kitty -e nvim "$UserConfigs/Laptops.conf"
             ;;
-        7)
+        *"View User-Settings")
             kitty -e nvim "$UserConfigs/UserSettings.conf"
             ;;
-        8)
+        *"View Workspace-Rules")
             kitty -e nvim "$UserConfigs/WorkspaceRules.conf"
             ;;
-		9)
+        *"View Default-Settings")
             kitty -e nvim "$configs/Settings.conf"
             ;;
-        10)
+        *"View Default-Keybinds")
             kitty -e nvim "$configs/Keybinds.conf"
             ;;
         *)

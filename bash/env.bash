eed4ae909cb3c7a36a8da2eabb151dcada16f020
adii-b <adityabharadwaj273@gmail.com>
2024-06-27 22:20:04 +0530

    removed old dotfiles

--- bash/env.bash
+++
@@ -1,4 +0,0 @@
-# CUSTOM PATHS
-export PATH="$PATH:/home/aditya/.dotnet/tools"
-export EDITOR='nvim'
-export MICRO_TRUECOLOR=1
--- bash/keybindings.bash
+++
@@ -1,3 +0,0 @@
-bind '"\e[A":history-search-backward'
-bind '"\e[B":history-search-forward'
-bind -s 'set completion-ignore-case on'

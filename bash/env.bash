69f79550d2bd02faff9018e1f0a358f6e4ce2b00
adii-b <adityabharadwaj273@gmail.com>
2024-07-25 23:12:31 +0530

    minor changes

--- bash/env.bash
+++ bash/env.bash
@@ -1,5 +1,5 @@
 # CUSTOM PATHS
-export PATH="$PATH:/home/aditya/.dotnet/tools"
 export EDITOR='nvim'
 export MICRO_TRUECOLOR=1
 export WARP_THEMES_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/warp-terminal/themes"
+export PATH="$PATH:/home/aditya/.dotnet/tools:/home/aditya/.local/bin"

e2d486436dc4ea24a2b23534b84b1106146ce478
adii-b <adityabharadwaj273@gmail.com>
2024-07-28 09:13:33 +0530

    tmux and many other changes

--- qtile/autostart.sh
+++
@@ -1,5 +0,0 @@
-picom --daemon &
-feh --bg-scale ~/Pictures/wallpapers/ghibli-japanese-walled-garden.png
-xrdb -merge ~/.Xresources &
-nm-applet &
-blueman-applet &
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,3 +4,4 @@ export EDITOR='nvim'
 export PATH="$PATH:/home/aditya/.dotnet/tools:/home/aditya/.local/bin"
 export WARP_THEMES_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/warp-terminal/themes"
 export GTK_THEME=$(gsettings get org.gnome.desktop.interface gtk-theme)
+export XDG_CONFIG_HOME="$HOME/.config/"

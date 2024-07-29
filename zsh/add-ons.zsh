95040eebf9502fe3e57ef0bf95002563cf69ca92
adii-b <adityabharadwaj273@gmail.com>
2024-07-30 00:07:18 +0530

    Minor changes

--- zsh/add-ons.zsh
+++ zsh/add-ons.zsh
@@ -3,6 +3,8 @@ source <(fzf --zsh)
 
 eval "$(zoxide init zsh)"
 # eval "$(oh-my-posh init zsh --config ~/.cache/oh-my-posh/themes/emodipt-extend.omp.json)"
+eval "$(oh-my-posh init zsh --config ~/.cache/oh-my-posh/themes/atomic.omp.json)"
+
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000

1584a2c163561b3882ab142ca2387d4d9d05aa10
adii-b <adityabharadwaj273@gmail.com>
2024-07-29 23:03:27 +0530

    Minor changes

--- bash/add-ons.bash
+++ bash/add-ons.bash
@@ -1,3 +1,3 @@
 eval "$(zoxide init bash)"
-# PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-eval "$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/emodipt-extend.omp.json)"
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+# eval "$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/emodipt-extend.omp.json)"

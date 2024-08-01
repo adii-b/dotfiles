6fa0dff92314a0b50dbf08228a0c51dd4c58480d
adii-b <adityabharadwaj273@gmail.com>
2024-08-01 16:51:56 +0530

    Minor changes

--- fish/add-ons/env.fish
+++
@@ -1,6 +0,0 @@
-set -x MICRO_TRUECOLOR 1
-set -x EDITOR nvim
-set -x PATH $PATH /home/aditya/.dotnet/tools /home/aditya/.local/bin
-set -x WARP_THEMES_DIR (echo $XDG_DATA_HOME)"/warp-terminal/themes"
-set -x GTK_THEME (gsettings get org.gnome.desktop.interface gtk-theme)
-set -x XDG_CONFIG_HOME "$HOME/.config/"

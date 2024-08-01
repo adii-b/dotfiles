34077aba30f0f05b38a1ba7c07a4d64629fa4f5a
adii-b <adityabharadwaj273@gmail.com>
2024-08-01 22:31:21 +0530

    removed starship

--- zsh/add-ons.zsh
+++ zsh/add-ons.zsh
@@ -3,7 +3,6 @@ source <(fzf --zsh)
 
 eval "$(zoxide init zsh)"
 # eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/atomic.omp.json)"
-eval "$(starship init zsh)"
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000

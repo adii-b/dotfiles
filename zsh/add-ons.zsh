9443ad510e963b2c7c0d03f685970245bf5cf792
adii-b <adityabharadwaj273@gmail.com>
2024-08-14 19:16:43 +0530

    minor changes

--- zsh/add-ons.zsh
+++ zsh/add-ons.zsh
@@ -1,6 +1,6 @@
 # Set-up FZF key bindings (CTRL R for fuzzy history finder)
 source <(fzf --zsh)
-
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 eval "$(zoxide init zsh)"
 # eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/atomic.omp.json)"
 

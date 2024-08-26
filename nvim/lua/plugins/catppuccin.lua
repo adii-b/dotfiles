67bc80b24cf263d6f272f068306590ef07e34ac6
adii-b <adityabharadwaj273@gmail.com>
2024-08-26 23:24:43 +0530

    minor tweaks

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,5 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { no_italic = true } },
 
   -- Configure LazyVim to load gruvbox
   {

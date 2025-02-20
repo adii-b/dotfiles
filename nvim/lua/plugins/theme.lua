2ff758efaed21d4c824854ffc79c508350297a86
adii-b <adityabharadwaj273@gmail.com>
2025-02-20 12:48:58 +0530

    slight theme changes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,5 +1,22 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- or "latte", "frappe", "macchiato"
+        no_italic = true, -- Disable italics globally
+        styles = {
+          comments = {}, -- No italics for comments
+          functions = {}, -- No italics for functions
+          keywords = {}, -- No italics for keywords
+          strings = {}, -- No italics for strings
+          variables = {}, -- No italics for variables
+        },
+      })
+    end,
+  },
 
   -- Configure LazyVim to load your favourite theme
   {

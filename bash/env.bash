ddb7e23471524cd966b6a5d1a860b2f5de9d0296
adii-b <adityabharadwaj273@gmail.com>
2025-03-15 23:21:32 +0530

    minor changes

--- bash/env.bash
+++ bash/env.bash
@@ -2,4 +2,6 @@
 export EDITOR='nvim'
 export MICRO_TRUECOLOR=1
 export WARP_THEMES_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/warp-terminal/themes"
-export PATH="$PATH:/home/aditya/.dotnet/tools:/home/aditya/.local/bin"
+export PATH="$PATH:/home/aditya/.dotnet/tools:/home/aditya/.local/bin:/home/aditya/.npm-global/bin"
+export GTK_THEME=$(gsettings get org.gnome.desktop.interface gtk-theme)
+export XDG_CONFIG_HOME="$HOME/.config/"

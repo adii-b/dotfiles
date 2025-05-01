6acde4d7efddf3a2c3a37912e5f771b8e0c8d24e
adii-b <adityabharadwaj273@gmail.com>
2025-05-02 00:48:43 +0530

    lot of changes

--- hypr/scripts/ClipManager.sh
+++ hypr/scripts/ClipManager.sh
@@ -6,7 +6,7 @@
 history=$(cliphist list | awk '{$1=""; print substr($0, 2)}')
 
 # Display clipboard history using rofi and capture the selected entry
-selected=$(echo "$history" | rofi -dmenu -i -p "Clipboard History:")
+selected=$(echo "$history" | rofi -dmenu -i -p "Clipboard History:" -theme ~/.config/rofi/launchers/type-7/style-1.rasi)
 
 # Copy the selected item to the clipboard if not empty
 if [ -n "$selected" ]; then
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,27 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--       require("tokyonight").setup({
+--         style = "night",
+--       })
+--       -- load the colorscheme here
+--       vim.cmd([[colorscheme tokyonight]])
+--     end,
+--   },
+-- }
 return {
   {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- You can adjust the priority if necessary
     config = function()
-      require("tokyonight").setup({
-        style = "night",
+      -- Set up the Catppuccin colorscheme with the "mocha" flavor
+      require("catppuccin").setup({
+        flavour = "mocha", -- Choose "latte", "frappe", "macchiato", or "mocha"
       })
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      -- Apply the colorscheme
     end,
   },
 }
